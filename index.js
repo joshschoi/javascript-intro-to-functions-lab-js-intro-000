87f7ee5406c0eec3440df8990c58ad838d504125
Joshua Choi <jsc.r839@gmail.com>
2019-06-06 23:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
   } else if (string === string.toUpperCase()){
-    return "YEST INDEED"
+    return "YES INDEED!"
   } else {
     return "I love you, too."
   }
